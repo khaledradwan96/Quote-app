0d0f93fdf858254777639a8e7b7ab2ac58f1b25b
Khaled Radwan <khaledradwan96@gmail.com>
2024-05-16 21:08:14 +0300

    add quotes add test app

--- js/main.js
+++ js/main.js
@@ -12,5 +12,24 @@ function getQuote() {
     } while (random === getQuote.last);
     getQuote.last = random;
     console.log("Random Number is => " + random); // Just to check 
+    
+    // get Quote with random number
+    quoteOutput.innerHTML = quotes[random].theQuote;
+    quoteAuthor.innerHTML = "~ " + quotes[random].theAuthor;
 };
+
+// 11 quotes with its author
+var quotes = [
+    {theQuote: "Be yourself; everyone else is already taken.", theAuthor: "Oscar Wilde"},
+    {theQuote: "In three words I can sum up everything I've learned about life: it goes on.", theAuthor: "Robert Frost"},
+    {theQuote: "So many books, so little time.", theAuthor: "Frank Zappa"},
+    {theQuote: "Live as if you were to die tomorrow. Learn as if you were to live forever.", theAuthor: "Mahatma Gandhi"},
+    {theQuote: "A room without books is like a body without a soul.", theAuthor: "Marcus Tullius Cicero"},
+    {theQuote: "To live is the rarest thing in the world. Most people exist, that is all.", theAuthor: "Oscar Wilde"},
+    {theQuote: "If you tell the truth, you don't have to remember anything.", theAuthor: "Mark Twain"},
+    {theQuote: "A friend is someone who knows all about you and still loves you.", theAuthor: "Elbert Hubbard"},
+    {theQuote: "You only live once, but if you do it right, once is enough.", theAuthor: "Mae West"},
+    {theQuote: "Be the change that you wish to see in the world.", theAuthor: "Mahatma Gandhi"},
+    {theQuote: "Good friends, good books, and a sleepy conscience: this is the ideal life.", theAuthor: "Mark Twain"},
+]
 
